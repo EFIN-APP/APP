0192cda9f8b51443f633d485a4b29ea4b5cd9c85
efinpartners-creator <efin.partners@gmail.com>
2025-08-28 19:30:16 -0300

    Delete lib directory

--- lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const OBJECTIVES = ["Ahorrar", "Invertir", "Salir de deudas", "Otro"];
-export const LEARNING_PREFERENCES = ["Videos", "Juegos", "Lecturas", "Simuladores"];
-export const INTERESTS = ["Cripto", "Inversión", "Economía básica", "VC", "Impuestos", "Deuda"];
